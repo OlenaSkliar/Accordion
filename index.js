4d604a43c22c2e15dcb0e043f3f701e7556ae418
OlenaSkliar <99468750+OlenaSkliar@users.noreply.github.com>
2023-04-08 13:05:57 +0300

    new change

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ let acc = document.getElementsByClassName("accordion__heading");
             this.closest('.accordion__item').classList.toggle("active");
             let accBody = this.closest('.accordion__item').querySelector('.accordion__body');
             if (this.closest('.accordion__item').classList.contains("active")) {
-                if (this.closest('.menu-mob')){
-                    accBody.style.height = accBody.querySelector('.accordion__info').offsetHeight + 20 + "px";
-                    this.closest('.menu-mob').style.height = 'auto'
-                }else{
-                    accBody.style.height = accBody.querySelector('.accordion__info').offsetHeight + 30 + "px"
-                }
+                accBody.style.height = accBody.querySelector('.accordion__info').offsetHeight + 20 + "px"
             } else {
                 accBody.style.height = '0'
             }
