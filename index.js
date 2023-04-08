4b4c770953bb0b8d582b41906750a458fbf644df
OlenaSkliar <99468750+OlenaSkliar@users.noreply.github.com>
2023-04-08 13:07:00 +0300

    new height

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let acc = document.getElementsByClassName("accordion__heading");
             this.closest('.accordion__item').classList.toggle("active");
             let accBody = this.closest('.accordion__item').querySelector('.accordion__body');
             if (this.closest('.accordion__item').classList.contains("active")) {
-                accBody.style.height = accBody.querySelector('.accordion__info').offsetHeight + 20 + "px"
+                accBody.style.height = accBody.querySelector('.accordion__info').offsetHeight + 10 + "px"
             } else {
                 accBody.style.height = '0'
             }
